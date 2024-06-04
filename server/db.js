7a718284eeee586183817752bda8cdac03415d1c
venoMouse <steninkgeorge007@gmail.com>
2024-06-04 19:34:09 +0530

    vercel config

--- server/db.js
+++ server/db.js
@@ -3,9 +3,7 @@ require("dotenv").config()
 
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL
-    
-
+    connectionString:process.env.POSTGRES_URL_NO_SSL
 })
 
 module.exports=pool
