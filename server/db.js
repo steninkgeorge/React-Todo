0d6c97bd31d98a8d5888df80e2e65e8eeb09ccae
venoMouse <steninkgeorge007@gmail.com>
2024-06-04 19:53:51 +0530

    config

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 
 const pool = new Pool({
-    connectionString:process.env.POSTGRES_URL_NO_SSL
+    connectionString:process.env.POSTGRES_URL
 })
 
 module.exports=pool
